5e004b59ebe1a7729d3f399a8f96ba284ed745d0
Isaac Wasserman <iwasserblaster@gmail.com>
2015-11-19 08:43:32 -0500

    fixed /day route

---
+++ public/daybroken.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+var dayurl = 'https://www.google.com/calendar/feeds/2smi71ngo1stlvs3dskc0q0io7bhpm3m%40import.calendar.google.com/public/basic?orderby=starttime&sortorder=ascending&futureevents=true&alt=json';
+
+var getCalendar = function(callback) {
+  request(dayurl, function(err, res, body) {
+//    console.log(body);
+    callback(err, JSON.parse(body));
+  });
+}
+
+router.get('/', function(req, res){
+  getCalendar(function(err, data){
+    var result = data;
+    var events = [];
+  
+  for(i=0; i < result.feed.entry.length; i++){
+    events.push(result.feed.entry[i]);
+  }
+  
+//  console.log(events);
+  
+  var eventstoday = [];
+  
+  var splitdate = Date().split(" ");
+  
+  var googledate =  [splitdate[0], splitdate[1], parseInt(splitdate[2]) + ","];
+//  var googledate =  ["Mon", "Nov", 16 + ","];
+  
+  for(i=0; i < events.length; i++){
+    var eventdate = events[i].summary.$t.split(" ").slice(1,4);
+    
+    console.log(eventdate);
+    console.log(googledate);
+    if(eventdate.toString() === googledate.toString()){
+      eventstoday.push(events[i]);
+    }
+  }
+  
+  var scheduleday = "";
+  
+  for(i=0; i < eventstoday.length; i++){
+    if(eventstoday[i].title.$t.search("Day 1") != -1 || eventstoday[i].title.$t.search("Day 2") != -1 || eventstoday[i].title.$t.search("Day 3") != -1 || eventstoday[i].title.$t.search("Day 4") != -1 || eventstoday[i].title.$t.search("Day 5") != -1 || eventstoday[i].title.$t.search("Day 6") != -1){
+      console.log(eventstoday[i].title.$t);
+      var thedayevent = eventstoday[i].title.$t;
+    }
+  }
+    if(thedayevent == null){
+      res.json("Today is not a school day.");
+    }
+    else {
+      res.json(thedayevent);
+    }
+  });
+});
+
+module.exports = router;
--- routes/day.js
+++ routes/day.js
@@ -1,57 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var cheerio = require('cheerio');
 
-var dayurl = 'https://www.google.com/calendar/feeds/2smi71ngo1stlvs3dskc0q0io7bhpm3m%40import.calendar.google.com/public/basic?orderby=starttime&sortorder=ascending&futureevents=true&alt=json';
-
-var getCalendar = function(callback) {
-  request(dayurl, function(err, res, body) {
-    callback(err, JSON.parse(body));
-  });
-}
+var url = "http://www.friends-select.org/calendars/index.aspx";
 
 router.get('/', function(req, res){
-  getCalendar(function(err, data){
-    var result = data;
-    var events = [];
-  
-  for(i=0; i < result.feed.entry.length; i++){
-    events.push(result.feed.entry[i]);
-  }
-  
-//  console.log(events);
-  
-  var eventstoday = [];
-  
-  var splitdate = Date().split(" ");
-  
-  var googledate =  [splitdate[0], splitdate[1], parseInt(splitdate[2]) + ","];
-//  var googledate =  ["Mon", "Nov", 16 + ","];
-  
-  for(i=0; i < events.length; i++){
-    var eventdate = events[i].summary.$t.split(" ").slice(1,4);
-    
-    console.log(eventdate);
-    console.log(googledate);
-    if(eventdate.toString() === googledate.toString()){
-      eventstoday.push(events[i]);
+  request(url,function(err,res2,body){
+    var $ = cheerio.load(body);
+    var events  = [];
+    var $events = $('.calendar-day dd h4');
+    for(var x = 0; x < $events.length; x++) {
+      d = $($events[x]).text();
+      events.push(d);
     }
-  }
-  
-  var scheduleday = "";
-  
-  for(i=0; i < eventstoday.length; i++){
-    if(eventstoday[i].title.$t.search("Day 1") != -1 || eventstoday[i].title.$t.search("Day 2") != -1 || eventstoday[i].title.$t.search("Day 3") != -1 || eventstoday[i].title.$t.search("Day 4") != -1 || eventstoday[i].title.$t.search("Day 5") != -1 || eventstoday[i].title.$t.search("Day 6") != -1){
-      console.log(eventstoday[i].title.$t);
-      var thedayevent = eventstoday[i].title.$t;
+    var dayevents = [];
+    for(i=0;i<events.length;i++){
+      if(events[i].length == 5 && events[i].indexOf("Day") != -1){
+        dayevents.push(events[i]);
+      }
     }
-  }
-    if(thedayevent == null){
-      res.json("Today is not a school day.");
-    }
-    else {
-      res.json(thedayevent);
-    }
+    res.send(dayevents[0]);
   });
 });
 
